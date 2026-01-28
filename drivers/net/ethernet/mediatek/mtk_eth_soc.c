fc44b11c6870c9a3f8e87d2d8eb2c86105d8de56
Alexander Riabchenko <d3adme4t@gmail.com>
2026-01-27 23:03:26 -0800

    net: ethernet: mtk_eth_soc: use explicit buffer size for page_pool creation
    Pass ring->buf_size explicitly to mtk_create_page_pool instead of relying on eth->rx_buf_len. This ensures the page_pool parameters (max_len and order) exactly match the buffer requirements calculated in mtk_rx_alloc, accounting for alignment and padding.

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -1989,17 +1989,16 @@ static bool mtk_page_pool_enabled(struct mtk_eth *eth)
 
 static struct page_pool *mtk_create_page_pool(struct mtk_eth *eth,
 					      struct xdp_rxq_info *xdp_q,
-					      int id, int size)
+					      int id, int size, int buf_size)
 {
 	struct page_pool_params pp_params = {
-		.order = get_order(eth->rx_buf_len + MTK_PP_HEADROOM),
+		.order = get_order(buf_size + MTK_PP_HEADROOM),
 		.flags = PP_FLAG_DMA_MAP | PP_FLAG_DMA_SYNC_DEV,
 		.pool_size = size,
 		.nid = NUMA_NO_NODE,
 		.dev = eth->dma_dev,
 		.offset = MTK_PP_HEADROOM,
-		.max_len = PAGE_SIZE
-			   << get_order(eth->rx_buf_len + MTK_PP_HEADROOM),
+		.max_len = buf_size,
 	};
 	struct page_pool *pp;
 	int err;
@@ -2939,7 +2938,7 @@ static int mtk_rx_alloc(struct mtk_eth *eth, int ring_no, int rx_flag)
 		struct page_pool *pp;
 
 		pp = mtk_create_page_pool(eth, &ring->xdp_q, ring_no,
-					  rx_dma_size);
+					  rx_dma_size, ring->buf_size);
 		if (IS_ERR(pp))
 			return PTR_ERR(pp);
 
